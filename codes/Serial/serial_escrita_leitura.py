17ee5875a2f5181478b933d7f02405e4c3c7beae
Mateus Antonio da Silva <mateus.antonio@eng.ci.ufpb.br>
2020-12-02 16:09:11 -0300

    Update serial_escrita_leitura.py

--- codes/Serial/serial_escrita_leitura.py
+++ codes/Serial/serial_escrita_leitura.py
@@ -1,11 +1,16 @@
-import serial
+# Importa biblioteca para acessar as portas Seriais
+from serial import Serial
 
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=1)
+# Inicia conexão na porta em que o Arduino está plugado
+arduino = Serial(port='COM4', baudrate=9600, timeout=1)
 
 while True:
+    # Espera mensagem do usuário
     msg = input('Envie um comando: ')
 
+    # Envia mensagem do usuário
     arduino.write(bytes(msg, encoding='utf-8'))
 
+    # Lê mensagem de retorno do Arduino e imprime na tela
     retorno = arduino.readline().decode('ascii')
     print('Retorno: ' + retorno)
